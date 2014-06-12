44fcd3a9ff9adf89b0c85848caf446611976528a
Ruben Gamez <rgamez@gmail.com>
2014-06-12 10:24:10 +0000

    Decorate $.show and $.hide to update aria-hidden property

--- src/js/former/jquery.former.accessibility.js
+++ src/js/former/jquery.former.accessibility.js
@@ -1,17 +1,17 @@
 var Accesibility = function(options){
 	var defaults = {
-		msg_mailto: ",will open your email client in a new window",
+		msg_mailto: ",will open your email client in a new window"
 	};
 
 	this.options = $.extend(defaults, options || {});
-}
+};
 
 /*
 	Apply some accessibility rules to the document
 */
 Accesibility.prototype.apply = function(){
 	this.mailto();
-}
+};
 
 /*
 	Inject an alt text into the mailto links notifying the user that it's
@@ -24,8 +24,38 @@ Accesibility.prototype.mailto = function(){
 
 	$("a").each(function(index){
 		$this = $(this);
-		if(this.href.startsWith('mailto:')){
+		if(this.href.match('^mailto:')){
 			$this.append(gif);
 		}
 	});
-}
+};
+
+/*
+	Decorate jQuery.show and jQuery.hide to update the aria-hiden attribute
+*/
+(function($)
+{
+    var _show = $.fn.show;
+    var _hide = $.fn.hide;
+ 
+    $.fn.show = function()
+    {
+        var ret = _show.apply(this, arguments);
+        if(this.attr('aria-hidden') && this.attr('aria-hidden') != 'false'){
+        	this.attr('aria-hidden', 'false')
+        }
+        return ret;
+    };
+
+    $.fn.hide = function()
+    {
+        var ret = _hide.apply(this, arguments);
+        if(this.attr('aria-hidden') && this.attr('aria-hidden') != 'true'){
+        	this.attr('aria-hidden', 'true')
+        }
+        return ret;
+    };
+
+})(jQuery);
+
+
